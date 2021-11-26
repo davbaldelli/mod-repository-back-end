df00e0ad8d4bf7c4c1d39667d6d33a717502aa90
Davide Baldelli <davbaldelli@gmail.com>
2021-11-26 19:48:14 +0100

    corrected error

--- models/db/models.go
+++ models/db/models.go
@@ -78,7 +78,7 @@ type Manufacturer struct {
 type Nation struct {
 	Id     uint           `gorm:"primarykey"`
 	Name   string
-	Code   string
+	Code   string			`gorm:"type:varchar(6)"`
 	Brands []Manufacturer `gorm:"foreignKey:IdNation"`
 	Tracks []Track        `gorm:"foreignKey:IdNation"`
 }
