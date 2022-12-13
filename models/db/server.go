49579fda1ba55db36e95591ed00056957bf9d666
Davide Baldelli <davbaldelli@gmail.com>
2022-12-13 14:15:30 +0100

    added outside track

--- models/db/server.go
+++ models/db/server.go
@@ -3,22 +3,24 @@ package db
 import "github.com/davide/ModRepository/models/entities"
 
 type Server struct {
-	Id          uint `gorm:"primaryKey"`
-	Name        string
-	Description string
-	JoinLink    string
-	Password    string
-	Online      bool
-	TrackId     uint
-	Cars        []*Car `gorm:"many2many:server_cars;foreignKey:Id;joinForeignKey:ServerId;References:Id;joinReferences:CarId"`
-	OutsideCars []*OutsideMod `gorm:"foreignKey:ServerId"`
+	Id               uint `gorm:"primaryKey"`
+	Name             string
+	Description      string
+	JoinLink         string
+	Password         string
+	Online           bool
+	TrackId          uint
+	Cars             []*Car        `gorm:"many2many:server_cars;foreignKey:Id;joinForeignKey:ServerId;References:Id;joinReferences:CarId"`
+	OutsideCars      []*OutsideMod `gorm:"foreignKey:ServerId"`
+	OutsideTrackName string
+	OutsideTrackLink string
 }
 
 type OutsideMod struct {
-	Id string `gorm:"primaryKey"`
-	Name string
+	Id           string `gorm:"primaryKey"`
+	Name         string
 	DownloadLink string
-	ServerId uint
+	ServerId     uint
 }
 
 func (s Server) ToEntity() entities.Server {
@@ -29,23 +31,25 @@ func (s Server) ToEntity() entities.Server {
 	for _, dbCar := range s.Cars {
 		cars = append(cars, dbCar.Id)
 	}
-	for _, outsideCar := range s.OutsideCars{
+	for _, outsideCar := range s.OutsideCars {
 		outsideCars = append(outsideCars, outsideCar.ToEntity())
 	}
 	return entities.Server{
-		Id:          s.Id,
-		Name:        s.Name,
-		Description: s.Description,
-		JoinLink:    s.JoinLink,
-		Password:    s.Password,
-		Online:      s.Online,
-		Track:       s.TrackId,
-		Cars:        cars,
-		OutsideCars: outsideCars,
+		Id:               s.Id,
+		Name:             s.Name,
+		Description:      s.Description,
+		JoinLink:         s.JoinLink,
+		Password:         s.Password,
+		Online:           s.Online,
+		Track:            s.TrackId,
+		OutsideTrackName: s.OutsideTrackName,
+		OutsideTrackLink: s.OutsideTrackLink,
+		Cars:             cars,
+		OutsideCars:      outsideCars,
 	}
 }
 
-func OutsideModFromEntity(mod entities.OutsideMod, serverId uint) OutsideMod{
+func OutsideModFromEntity(mod entities.OutsideMod, serverId uint) OutsideMod {
 	return OutsideMod{
 		Name:         mod.Name,
 		DownloadLink: mod.DownloadLink,
@@ -53,7 +57,7 @@ func OutsideModFromEntity(mod entities.OutsideMod, serverId uint) OutsideMod{
 	}
 }
 
-func (o OutsideMod) ToEntity() entities.OutsideMod{
+func (o OutsideMod) ToEntity() entities.OutsideMod {
 	return entities.OutsideMod{
 		Id:           o.Id,
 		Name:         o.Name,
--- models/entities/server.go
+++ models/entities/server.go
@@ -1,20 +1,22 @@
 package entities
 
 type Server struct {
-	Id          uint   `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	JoinLink    string `json:"joinLink"`
-	Password    string `json:"password"`
-	Online      bool   `json:"online"`
-	Track       uint   `json:"track"`
-	Cars        []uint `json:"cars"`
-	OutsideCars []OutsideMod `json:"outsideCars"`
+	Id               uint         `json:"id"`
+	Name             string       `json:"name"`
+	Description      string       `json:"description"`
+	JoinLink         string       `json:"joinLink"`
+	Password         string       `json:"password"`
+	Online           bool         `json:"online"`
+	Track            uint         `json:"track"`
+	Cars             []uint       `json:"cars"`
+	OutsideTrackName string       `json:"outsideTrackName"`
+	OutsideTrackLink string       `json:"outsideTrackLink"`
+	OutsideCars      []OutsideMod `json:"outsideCars"`
 }
 
 type OutsideMod struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
+	Id           string `json:"id"`
+	Name         string `json:"name"`
 	DownloadLink string `json:"downloadLink"`
-	ServerId uint `json:"serverId"`
+	ServerId     uint   `json:"serverId"`
 }
